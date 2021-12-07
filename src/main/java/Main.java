493ba884622e6ae4e05c5d8d92e0a96e142eb082
sophiahoelzl <sophia.hoelzl02@gmail.com>
2021-12-07 12:06:35 +0100

    kleine änderung

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     //ist eher dann wenn das Game schon steht
     public static void main (String[] args){
         System.out.println("Hello World!");
-        // Test Sophia:)
     }
 }
