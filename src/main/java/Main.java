aa2c991d11df041216249fcffc8ba644b35d96d8
sarah <sara.hoeferer @gmx.net>
2021-12-07 10:56:15 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //HelloFX ist eine JavaFX Klasse, einfach mal ignoieren, werden am Anfang eh noch nicht wirklich damit arbeiten
+    //ist eher dann wenn das Game schon steht
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
