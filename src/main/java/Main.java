12e195b0fb4ab7b723842d6567bcf07f08b1f918
sarah <sara.hoeferer @gmx.net>
2021-12-15 10:06:39 +0100

    Commit Test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     //HelloFX ist eine JavaFX Klasse, einfach mal ignorieren, werden am Anfang eh noch nicht wirklich damit arbeiten
     //ist eher dann, wenn das Game schon steht
     public static void main (String[] args){
-        System.out.println("Hello Worl!");
+        System.out.println("Hello Wor!");
     }
 }
