941b0e249fe12f67ccb2bb579abbef4716374275
sophiahoelzl <sophia.hoelzl02@gmail.com>
2022-01-12 16:47:20 +0100

    Player Klasse kommentiert

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
 public class Player {
+    //private Klassenvariablen
     private char answer;
     private int money;
     private int kategorie;
 
-    //Setter/Konstruktor
+    //Konstruktor
     public Player() {
         this.answer = '\0';
         this.money = 0;
         this.kategorie = 1;
     }
 
-    //Getter
+    //Gettermethode für Antwort
     public char getAnswer() {
         return answer;
     }
 
+    //Gettermethode um Kategorie zu bekommen
     public int getKategorie(){
         return kategorie;
     }
 
-    //Methoden
+    //Methode um eine Antwortmöglichkeit einzugeben
     public void makeGuess(Scanner scanIn) {
         answer = '\0';
         while (answer != 'a' && answer != 'b' && answer != 'c' && answer != 'd') {
@@ -30,6 +32,8 @@ public class Player {
         }
     }
 
+
+    //Methode zur Abfrage nach Joker
     public void chooseJoker(Scanner scanIn) {
         answer = '\0';
         while (answer != 'f' && answer != 'h' && answer != 's' && answer != 'k') {
@@ -38,6 +42,7 @@ public class Player {
         }
     }
 
+    //Methode zur Abfrage ob man das Spiel verlassen möchte
     public boolean leave(Scanner scanIn) {
         answer = '\0';
         while (answer != 'y' && answer != 'n') {
@@ -47,12 +52,14 @@ public class Player {
         return answer == 'y';
     }
 
+    //Methode um die Kategorie nach jeder Frage um eins zu erhöhen
     public void raiseCategory() {
         kategorie++;
     }
 
+    //Methode um aus Kategorie den Geldbetrag zu ermitteln
     public void switchMoney() {
-        switch (kategorie) {
+        switch (kategorie) { //Kategorie wird geswitched
             case 2:
                 money = 50;
                 break;
@@ -104,20 +111,22 @@ public class Player {
         }
     }
 
+    //Methode um das Geld nach jeder Frage bezüglich der Kategorie auf Konsole auszugeben
     public void printMoney() {
         this.switchMoney();
         System.out.println("Du stehst bei " + money + "€!");
     }
 
+    //Methode um Geld nach Ende des Spiels auszugeben
     public void printMoneyWon(boolean leave) {
         System.out.println();
         if (leave){
             System.out.println("Du hast " + money + "€ gewonnen!");
-        } else if (kategorie < 6) {
+        } else if (kategorie < 6) { //wenn du eine Frage falsch beantwortet hast und nicht weiter als Kategorie 4 gekommen bist
             System.out.println("Du hast 0€ gewonnen!");
-        } else if (kategorie < 11) {
+        } else if (kategorie < 11) { //wenn du eine Frage falsch beantwortet hast und nicht weiter als Kategorie 9 gekommen bist
             System.out.println("Du hast 500€ gewonnen!");
-        } else if (kategorie >= 11) {
+        } else if (kategorie >= 11) { //wenn du eine Frage falsch beantwortet hast und höher als Kategorie 10 warst
             System.out.println("Du hast 16000€ gewonnen!");
         }
     }
